ae43d4efb6ae431d524004529411a883a010dee1
Ayush-Gala <ayushgala2@gmail.com>
2022-11-25 07:12:22 +0530

    Count Down Timer

---
+++ main.js
@@ -0,0 +1,49 @@
+// Global variables
+const time_el = document.querySelector('.watch .time');
+const start_btn = document.getElementById('start');
+const stop_btn = document.getElementById("stop");
+const reset_btn = document.getElementById("reset");
+
+let seconds = 60;	//change number of seconds here
+let interval = null;
+
+// Event listeners
+start_btn.addEventListener('click', start);
+stop_btn.addEventListener("click", stop);
+reset_btn.addEventListener("click", reset);
+
+// Update the timer
+function timer () {
+	seconds--;
+
+	// Format our time
+	let hrs = Math.floor(seconds / 3600);
+	let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+	let secs = seconds % 60;
+
+	if (secs < 10) secs = '0' + secs;
+	if (mins < 10) mins = "0" + mins;
+	if (hrs < 10) hrs = "0" + hrs;
+
+	//time_el.innerText = `${hrs}:${mins}:${secs}`; //to display hh:mm:ss
+	time_el.innerText = `${mins}:${secs}`;
+}
+
+function start () {
+	if (interval) {
+		return
+	}
+
+	interval = setInterval(timer, 1000);
+}
+
+function stop () {
+	clearInterval(interval);
+	interval = null;
+}
+
+function reset () {
+	stop();
+	seconds = 60;	//change number of seconds here
+	time_el.innerText = '01:00';	//change look here
+}
